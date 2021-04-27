5e4e76c8637f7e11f621b26558624b0b9c290b4c
Jin0331 <sempre813@hallym.ac.kr>
2021-04-27 10:50:22 +0900

    update

--- tophat_cuff_pipe.sh
+++ tophat_cuff_pipe.sh
@@ -2,8 +2,8 @@
 
 # container name
 # tophat, cufflink
-# outdir <---- in mounted point, mkdir -p [temp]/Tophat_result
-# outdir <---- in mounted point, mkdir -p [temp]/cufflinks_result
+# outdir <---- in mounted point, mkdir -p [outdir]/Tophat_result
+# outdir <---- in mounted point, mkdir -p [outdir]/cufflinks_result
 
 # e.g) RNA_script/tophat_cuff_pipe.sh -i /data/input_dir/temp -o /data/output_dir -x /data/index -t 8
 
@@ -18,9 +18,10 @@ do
     esac
 done
 
+# dir making
+docker exec -it tophat2 /bin/sh -c "mkdir -p ${outdir}/Tophat_result;mkdir -p ${outdir}/cufflinks_result"
+
 # tophat CMD
 docker exec tophat2 /bin/sh -c "/data/RNA_script/tophat_pipe.sh -i ${indir} -o ${outdir} -x ${indexdir} -t ${thread}"
-
-
 # cufflink CMD
 docker exec cufflink /bin/sh -c "/data/RNA_script/cufflink_pipe.sh -i ${outdir}/Tophat_result -o ${outdir} -x ${indexdir} -t ${thread}"
