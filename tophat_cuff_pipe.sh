f754628b3a4570ab017f9664f55ef335fc49f326
Jin0331 <sempre813@hallym.ac.kr>
2021-04-28 11:55:41 +0900

    update

--- tophat_cuff_pipe.sh
+++ tophat_cuff_pipe.sh
@@ -5,27 +5,24 @@
 # outdir <---- in mounted point, mkdir -p [outdir]/Tophat_result
 # outdir <---- in mounted point, mkdir -p [outdir]/cufflinks_result
 
-# e.g) RNA_script/tophat_cuff_pipe.sh -i /data/input_dir/temp -o /data/output_dir -x /data/index -t 8
+# e.g) RNA_script/tophat_cuff_pipe.sh -i /data/input_dir/temp -o /data/output_dir -x /data/index -t 8 -g fastq or fq
 
 # command line variable 
-while getopts i:o:x:t: flag
+while getopts i:o:x:t:g: flag
 do
     case "${flag}" in
         i) indir=${OPTARG};;
         o) outdir=${OPTARG};;
         x) indexdir=${OPTARG};;
         t) thread=${OPTARG};;
+        g) type=${OPTARG};;
     esac
 done
-
-# docker run
-#docker run --rm -dit -v ../${input_dir}:/data --name pipe_1 genomicpariscentre/tophat2:latest /bin/bash
-#docker run --rm -dit -v ../${input_dir}:/data --name pipe_2 fomightez/rnaseqcufflinks:latest /bin/bash
 
 # dir making
 docker exec -it tophat2 /bin/sh -c "mkdir -p ${outdir}/Tophat_result;mkdir -p ${outdir}/cufflinks_result"
 
 # tophat CMD
-docker exec tophat2 /bin/sh -c "/data/RNA_script/tophat_pipe.sh -i ${indir} -o ${outdir} -x ${indexdir} -t ${thread}"
+docker exec tophat2 /bin/sh -c "/data/RNA_script/tophat_pipe.sh -i ${indir} -o ${outdir} -x ${indexdir} -t ${thread} -g ${type}"
 # cufflink CMD
 docker exec cufflink /bin/sh -c "/data/RNA_script/cufflink_pipe.sh -i ${outdir}/Tophat_result -o ${outdir} -x ${indexdir} -t ${thread}"
--- tophat_pipe.sh
+++ tophat_pipe.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-while getopts i:o::x:t: flag
+while getopts i:o::x:t:g: flag
 do
     case "${flag}" in
         i) indir=${OPTARG};;
         o) outdir=${OPTARG};;
         x) indexdir=${OPTARG};;
         t) thread=${OPTARG};;
+        g) type=${OPTARG};;
     esac
 done
 
@@ -20,5 +21,5 @@ echo $file_list
 for file_name in $file_list
 do
     echo $file_name  #K1Aligned_Access_R1.fastq.gz
-    tophat2 --num-threads ${thread} -G ${indexdir}/gencode.v37.primary_assembly.annotation.gtf -o ${outdir}/Tophat_result/${file_name} ${indexdir}/hg38 ${indir}/${file_name}_R1.fastq.gz ${indir}/${file_name}_R2.fastq.gz
+    tophat2 --num-threads ${thread} -G ${indexdir}/gencode.v37.primary_assembly.annotation.gtf -o ${outdir}/Tophat_result/${file_name} ${indexdir}/hg38 ${indir}/${file_name}_R1.${type}.gz ${indir}/${file_name}_R2.${type}.gz
 done
