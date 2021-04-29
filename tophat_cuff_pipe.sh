a607e9436f2da7ca685f7ca84effb57283b22e8d
Jin0331 <sempre813@hallym.ac.kr>
2021-04-29 16:51:24 +0900

    update

--- tophat_cuff_pipe.sh
+++ tophat_cuff_pipe.sh
@@ -4,20 +4,21 @@
 # e.g) RNA_script/tophat_cuff_pipe.sh -i /data/input_dir/temp -o /data/output_dir -x /data/index -t 8 -g fastq or fq
 
 # command line variable 
-while getopts b:i:o:x:t:g: flag
+while getopts b:i:x:t:g: flag
 do
     case "${flag}" in
         b) basedir=${OPTARG};;
         i) indir=${OPTARG};;
-        o) outdir=${OPTARG};;
         x) indexdir=${OPTARG};;
         t) thread=${OPTARG};;
         g) type=${OPTARG};;
     esac
 done
 
-# RNA-Seq script
-git clone https://github.com/Jin0331/RNA_script.git ${basedir}/RNA_script
+# # RNA-Seq script
+# git clone https://github.com/Jin0331/RNA_script.git ${basedir}/RNA_script
+
+# index file download
 
 # tophat container
 docker run -dit -v ${basedir}:/data --name tophat2 genomicpariscentre/tophat2:latest /bin/bash
@@ -25,11 +26,11 @@ docker run -dit -v ${basedir}:/data --name tophat2 genomicpariscentre/tophat2:la
 docker run -dit -v ${basedir}:/data --name cufflink fomightez/rnaseqcufflinks:latest /bin/bash
 
 # dir making
-docker exec -it tophat2 /bin/sh -c "mkdir -p ${outdir}/Tophat_result;mkdir -p ${outdir}/cufflinks_result"
+docker exec -it tophat2 /bin/sh -c "mkdir -p /data/output_dir/Tophat_result;mkdir -p /data/output_dir/cufflinks_result;chmod -R 777 /data/output_dir"
 
 # tophat CMD
-docker exec tophat2 /bin/sh -c "/data/RNA_script/tophat_pipe.sh -i ${indir} -o ${outdir} -x ${indexdir} -t ${thread} -g ${type}"
+docker exec tophat2 /bin/sh -c "/data/RNA_script/tophat_pipe.sh -i /data/${indir} -o /data/output_dir -x /data/${indexdir} -t ${thread} -g ${type}"
 # cufflink CMD
-docker exec cufflink /bin/sh -c "/data/RNA_script/cufflink_pipe.sh -i ${outdir}/Tophat_result -o ${outdir} -x ${indexdir} -t ${thread}"
+docker exec cufflink /bin/sh -c "/data/RNA_script/cufflink_pipe.sh -i /data/output_dir/Tophat_result -o /data/output_dir -x /data/${indexdir} -t ${thread}"
 
 docker stop tophat2 cufflink && docker rm tophat2 cufflink 
