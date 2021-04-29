5f74559aab9453dd55c48df36bac7a3ad2d842c4
Jin0331 <sempre813@hallym.ac.kr>
2021-04-29 15:21:01 +0900

    update

--- tophat_cuff_pipe.sh
+++ tophat_cuff_pipe.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# container name
 # tophat, cufflink
-# outdir <---- in mounted point, mkdir -p [outdir]/Tophat_result
-# outdir <---- in mounted point, mkdir -p [outdir]/cufflinks_result
-
 # e.g) RNA_script/tophat_cuff_pipe.sh -i /data/input_dir/temp -o /data/output_dir -x /data/index -t 8 -g fastq or fq
 
 # command line variable 
-while getopts i:o:x:t:g: flag
+while getopts b:i:o:x:t:g: flag
 do
     case "${flag}" in
+        b) basedir=${OPTARG};;
         i) indir=${OPTARG};;
         o) outdir=${OPTARG};;
         x) indexdir=${OPTARG};;
@@ -18,6 +15,14 @@ do
         g) type=${OPTARG};;
     esac
 done
+
+# RNA-Seq script
+git clone https://github.com/Jin0331/RNA_script.git ${basedir}/RNA_script
+
+# tophat container
+docker run -dit -v ${basedir}:/data --name tophat2 genomicpariscentre/tophat2:latest /bin/bash
+# cufflink container
+docker run -dit -v ${basedir}:/data --name cufflink fomightez/rnaseqcufflinks:latest /bin/bash
 
 # dir making
 docker exec -it tophat2 /bin/sh -c "mkdir -p ${outdir}/Tophat_result;mkdir -p ${outdir}/cufflinks_result"
