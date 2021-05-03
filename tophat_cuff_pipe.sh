9fd23cd92d6853ad65517862cb97a3d63ac67908
Jin0331 <sempre813@hallym.ac.kr>
2021-05-03 10:09:07 +0900

    update

--- tophat_cuff_pipe.sh
+++ tophat_cuff_pipe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # tophat, cufflink
-#  RNA_script/tophat_cuff_pipe.sh -n ID -p PASSWORD -h NAS -b /home/wmbio/temp -i input_dir -x index -t 10 -g fastq
+# RUN ex) tophat_cuff_pipe.sh -n NASid -p NASpw -h NASip -b /home/wmbio/temp -i input_dir -x index -t 10 -g fq
 # command line variable 
 while getopts n:p:h:b:i:x:t:g: flag
 do
@@ -20,21 +20,41 @@ done
 # # RNA-Seq script
 # git clone https://github.com/Jin0331/RNA_script.git ${basedir}/RNA_script
 
+
 # index file download
-ncftpget -u ${nasid} -p ${naspw} ${nas} ${basedir} /Share/index.tar.gz
-tar -xzvf index.tar.gz
+if [ ! -d ${basedir}/index ];then
+    echo "No Index dir"
+    ncftpget -u ${nasid} -p ${naspw} ${nas} ${basedir} /Share/index.tar.gz
+    tar -xzvf index.tar.gz -C ${basedir}
+else
+    echo "Index dir exist!"
+fi
+
+
+## Docker run
+# trimgalore
+docker run --rm -dit -v ${basedir}:/data --name trim_galore dukegcb/trim-galore:latest /bin/bash
 
 # tophat container
 docker run --rm -dit -v ${basedir}:/data --name tophat2 genomicpariscentre/tophat2:latest /bin/bash
+
 # cufflink container
 docker run --rm -dit -v ${basedir}:/data --name cufflink fomightez/rnaseqcufflinks:latest /bin/bash
 
 # dir making
-docker exec -it tophat2 /bin/sh -c "mkdir -p /data/output_dir/Tophat_result;mkdir -p /data/output_dir/cufflinks_result;chmod -R 777 /data/output_dir"
+docker exec -it tophat2 /bin/sh -c "mkdir -p /data/output_dir/QC; mkdir -p /data/output_dir/Tophat_result;mkdir -p /data/output_dir/cufflinks_result;chmod -R 777 /data/output_dir"
+
+# trim_galore CMD
+echo "Trim Galore RUN!!!!!!!!"
+docker exec trim_galore /bin/sh -c "/data/RNA_script/trim_galore_pipe.sh -i /data/${indir} -o /data/output_dir"
 
 # tophat CMD
-docker exec tophat2 /bin/sh -c "/data/RNA_script/tophat_pipe.sh -i /data/${indir} -o /data/output_dir -x /data/${indexdir} -t ${thread} -g ${type}"
+echo "Tophat2 RUN!!!!!!!!"
+docker exec tophat2 /bin/sh -c "/data/RNA_script/tophat_pipe.sh -i /data/output_dir/QC -o /data/output_dir -x /data/${indexdir} -t ${thread} -g ${type}"
+
 # cufflink CMD
+echo "CUFFLINKS RUN!!!!!!!!"
 docker exec cufflink /bin/sh -c "/data/RNA_script/cufflink_pipe.sh -i /data/output_dir/Tophat_result -o /data/output_dir -x /data/${indexdir} -t ${thread}"
 
-docker stop tophat2 cufflink
+# done
+docker stop trim_galore tophat2 cufflink
--- trim_galore_pipe.sh
+++ trim_galore_pipe.sh
@@ -5,9 +5,6 @@ do
     case "${flag}" in
         i) indir=${OPTARG};;
         o) outdir=${OPTARG};;
-        x) indexdir=${OPTARG};;
-        t) thread=${OPTARG};;
-        g) type=${OPTARG};;
     esac
 done
 
@@ -21,5 +18,5 @@ echo $file_list
 for file_name in $file_list
 do
     echo $file_name  #K1Aligned_Access_R1.fastq.gz
-    tophat2 --num-threads ${thread} -G ${indexdir}/gencode.v37.primary_assembly.annotation.gtf -o ${outdir}/Tophat_result/${file_name} ${indexdir}/hg38 ${indir}/${file_name}_R1_val_1.${type}.gz ${indir}/${file_name}_R2_val_2.${type}.gz
+    trim_galore --no_report_file --q 30 --paired --clip_R1 10 --clip_R2 10 ${indir}/${file_name}_R1.fastq.gz ${indir}/${file_name}_R2.fastq.gz -o ${outdir}/QC 
 done
