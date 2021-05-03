69f1daf6afe7044101435a94f6f7bb2690481f8e
Jin0331 <sempre813@hallym.ac.kr>
2021-05-03 16:19:35 +0900

    update

---
+++ sra_fastq_downloader.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# argument
+basedir=$1
+filename=$2
+
+while read line; do
+# reading each line
+    echo $line
+    docker run --rm -v ${basedir}/:/data pegi3s/sratoolkit fastq-dump -F ${line} --outdir /data
+done < $filename
